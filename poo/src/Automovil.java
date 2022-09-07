65207a24ecac676a62c81fb9e40f5e5bdc78a030
alibertiux <ali.bertiux.872@gmail.com>
2022-09-07 10:44:16 -0500

    Add Enum and  static varible

--- poo/src/Automovil.java
+++ poo/src/Automovil.java
@@ -1,14 +1,57 @@
+package poo.src;
+
 public class Automovil {
-
-
+    // /*/*/*/*/*/*/*/*/*/*/* Atributos /*/*/*/*/*/*/*/*/*/*/*
+    private int id;
     private String fabricante;
     private String modelo;
-    private String color;
+    private static Color color = Color.GRIS;
     private double cilindrada;
     private int capacidadTanque = 40;
+    private TipoAutomovil tipo;
+    private static Color colorPatente = Color.NARANJO;
+    private static int capacidadEstanqueEstatico = 30;
+    private static int ultimoId;
+    public static final Integer VELOCIDAD_MAX_CARRETERA = 120;
+    public static final int VELOCIDAD_MAX_CIUDAD =60;
+
+    public static final String COLOR_ROJO = "Rojo";
+    public static final String COLOR_AMARILLO = "Amarillo";
+    public static final String COLOR_AZUL = "Azul";
+    public static final String COLOR_BLANCO = "Blanco";
+    public static final String COLOR_GRIS = "Gris Oscuro";
 
 
-    public String getFabricante(){
+
+    // /*/*/*/*/*/*/*/*/*/*/* Constructores /*/*/*/*/*/*/*/*/*/*/*
+    public Automovil() {
+        this.id = ++ultimoId;
+    }
+
+    public Automovil(String fabricante, String modelo) {
+        this();
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color) {
+        this(fabricante, modelo);
+        this.color = color;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, double cilindrada) {
+        this(fabricante, modelo, color);
+        this.cilindrada = cilindrada;
+    }
+
+    public Automovil(String fabricante, String modelo, Color color, double cilindrada, int capacidadTanque) {
+        this( fabricante, modelo, color, cilindrada);
+        this.capacidadTanque = capacidadTanque;
+    }
+
+
+    // /*/*/*/*/*/*/*/*/*/*/* Metodos getter and setter /*/*/*/*/*/*/*/*/*/*/*
+    public String getFabricante() {
         return fabricante;
     }
 
@@ -16,7 +59,7 @@ public class Automovil {
         this.fabricante = fabricante;
     }
 
-    public String getModelo(){
+    public String getModelo() {
         return modelo;
     }
 
@@ -24,14 +67,12 @@ public class Automovil {
         this.modelo = modelo;
     }
 
-    public String getColor(){
+    public Color getColor() {
         return color;
     }
-
-    public void setColor(String color) {
+    public void  setColor(Color color) {
         this.color = color;
     }
-
     public double getCilindrada() {
         return cilindrada;
     }
@@ -45,11 +86,43 @@ public class Automovil {
     }
 
     public void setCapacidadTanque(int capacidadTanque) {
-
+        this.capacidadTanque = capacidadTanque;
     }
 
-    //metodo detalle
-    public String  detalle() {
+    public static Color getColorPatente() {
+        return colorPatente;
+    }
+    public static void setColorPatente(Color colorPatente) {
+        Automovil.colorPatente = colorPatente;
+    }
+
+    public static int getCapacidadEstanqueEstatico() {
+        return capacidadEstanqueEstatico;
+    }
+
+    public static void setCapacidadEstanqueEstatico(int capacidadEstanqueEstatico) {
+        Automovil.capacidadEstanqueEstatico = capacidadEstanqueEstatico;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public TipoAutomovil getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoAutomovil tipo) {
+        this.tipo = tipo;
+    }
+
+    // /*/*/*/*/*/*/*/*/*/*/* Metodo de operacion /*/*/*/*/*/*/*/*/*/*/*
+    // metodo detalle
+    public String detalle() {
 
         /* Distintas posibilidades de retornar solo utilizar con grandes cadenas sp.append es mutable
         sb.append("auto.fabricante = " + this.fabricante);
@@ -67,35 +140,66 @@ public class Automovil {
         return sb;
         */
 
-        return "auto.fabricante = " + this.fabricante +
-                "\n auto.modelo = " + this.modelo +
+        return "auto.id = " + this.id +
+                "\n auto.fabricante = " + this.getFabricante() +
+                "\n auto.modelo = " + this.getModelo() +
+                "\n auto.tipo = " + this.getTipo().getDescripcion() +
                 "\n auto.color = " + this.color +
-                "\n auto.cilindrada = " + this.cilindrada;
-    }
-        // metodo acelerar
-        public String acelerar(int rpm){
-            return "el auto " + this.fabricante + " acelarando a " + rpm + " rpm";
-        }
-
-        // metodo frenanr
-        public String frenar(){
-            return fabricante + " " + this.modelo + " frenando!";
-        }
-        // metodo acelefar/frenar
-        public String acelerarFrenar(int rpm){
-            String acelerar = this.acelerar(rpm);
-            String frenar = this.frenar();
-                    return acelerar + "\n" + frenar;
-        }
-
-        // metodo calcular rendimiento
-        public float calcularConsumo(int km, float porcentajeBencina){
-        return km/(capacidadTanque*porcentajeBencina);
-        }
-         // metodo calcular rendimiento (sobrecarga de metodo / mismo nombre, distintos parametros
-        public float calcularConsumo(int km, int porcentajeBencina){
-        return km/(capacidadTanque*(porcentajeBencina/100f));
+                "\n auto.colorPatente = " + colorPatente +
+                "\n auto.cilindrada = " + this.getCilindrada();
     }
 
+
+
+    // metodo acelerar
+    public String acelerar(int rpm) {
+        return "el auto " + this.fabricante + " acelarando a " + rpm + " rpm";
+    }
+
+    // metodo frenanr
+    public String frenar() {
+        return fabricante + " " + this.modelo + " frenando!";
+    }
+
+    // metodo acelefar/frenar
+    public String acelerarFrenar(int rpm) {
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+    }
+
+    // metodo calcular rendimiento
+    public float calcularConsumo(int km, float porcentajeBencina) {
+        return km / (capacidadTanque * porcentajeBencina);
+    }
+
+    // metodo calcular rendimiento (sobrecarga de metodo / mismo nombre, distintos parametros
+    public float calcularConsumo(int km, int porcentajeBencina) {
+        return km / (capacidadTanque * (porcentajeBencina / 100f));
+    }
+
+    public static float calcularConsumoEstatico(int km, int porcentajeBencina){
+        return km / (capacidadEstanqueEstatico * (porcentajeBencina / 100f));
+    }
+    // /*/*/*/*/*/*/*/*/*/*/* Sobre escritura de metodos /*/*/*/*/*/*/*/*/*/*/*
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(!(obj instanceof Automovil)){
+            return false;
+        }
+        Automovil a = (Automovil) obj;
+        return (this.fabricante != null && this.modelo != null
+                && this.fabricante.equals(a.getFabricante())
+                && this.modelo.equals(a.getModelo()));
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " : " + fabricante + " " + modelo ;
+    }
 
 }
---
+++ poo/src/EjemploAutomovilEnum.java
@@ -0,0 +1,48 @@
+package poo.src;
+
+public class EjemploAutomovilEnum {
+    public static void main(String[] args) {
+
+        Automovil.setCapacidadEstanqueEstatico(45);
+
+        Automovil subaru = new Automovil("Subaru", "Impreza");
+        subaru.setCilindrada(2.0);
+        subaru.setColor(Color.BLANCO);
+        subaru.setTipo(TipoAutomovil.HATCHBACK);
+
+        Automovil mazda = new Automovil("Mazda", "BT-50", Color.ROJO,3.0);
+        mazda.setTipo(TipoAutomovil.PICKUP);
+        System.out.println(("mazda fabricante = "+ mazda.getFabricante()));
+
+        TipoAutomovil tipo = subaru.getTipo();
+        System.out.println("tipo Subaru = " + tipo.getNombre());
+        System.out.println("tipo desc Subaru = " + tipo.getDescripcion());
+
+
+        tipo = mazda.getTipo();
+        switch(tipo){
+            case CONVERTIBLE:
+                System.out.println("El automovil es deportivo y descapotable de dos puetas");
+                break;
+            case COUPE:
+                System.out.println("Es un automovil pequeño de dos puertas y tipicamente deportivo");
+                break;
+            case FURGON:
+                System.out.println("Es un automovil utilitario de transporte de empresas");
+                break;
+            case HATCHBACK:
+                System.out.println("Es un automovil mediano compacto, aspecto deportivo");
+                break;
+            case PICKUP:
+                System.out.println("Es un automovil de doble cabina o camioneta");
+                break;
+            case SEDAN:
+                System.out.println("Es un automovil mediano");
+                break;
+            case STATION_WAGON:
+                System.out.println("Es un automovil mas grande, con maleta grande");
+                break;
+        }
+
+    }
+}
